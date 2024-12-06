6131c7f9ea48d96bd3b96af104d7d4783f27d7c5
KhoaVo-dgkv <dangkhoavo.work@gmail.com>
2024-12-06 17:17:43 +0700

    Login

--- KTPOS Order/Login.Designer.cs
+++ KTPOS Order/Login.Designer.cs
@@ -160,6 +160,7 @@
             this.btnExit.TabIndex = 9;
             this.btnExit.Text = "Exit";
             this.btnExit.UseTransparentBackground = true;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
             // 
             // txtPass
             // 
