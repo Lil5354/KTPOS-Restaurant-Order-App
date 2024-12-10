9dbc2ff9a47980141475358656c87d01c8cdc655
Lil5354 <dttthao.5354@gmail.com>
2024-12-10 22:11:10 +0700

    Table Staff

---
+++ KTPOS Order/Proccess/BillProcessor.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KTPOS_Order.Proccess
+{
+    public class BillProcessor
+    {
+        public int BillID { get; private set; }
+        public DataTable BillDetails { get; private set; }
+
+        public BillProcessor(int billId)
+        {
+            BillID = billId;
+            LoadBillDetails();
+        }
+
+        private void LoadBillDetails()
+        {
+            string billDetailsQuery = @"
+                SELECT i.fname AS ItemName, i.price AS UnitPrice, bi.count AS Quantity,
+                       (i.price * bi.count) AS TotalPrice
+                FROM BILLINF bi
+                JOIN ITEM i ON bi.idFD = i.ID
+                WHERE bi.idBill = @billId";
+
+            BillDetails = GetDatabase.Instance.ExecuteQuery(billDetailsQuery, new object[] { BillID });
+        }
+    }
+}
+
