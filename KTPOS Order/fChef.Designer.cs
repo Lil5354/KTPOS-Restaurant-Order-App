b1a1ecd468f82cfedef69e1f1c04cddfbfcc25d4
Lil5354 <dttthao.5354@gmail.com>
2024-12-11 17:10:11 +0700

    fChef

---
+++ KTPOS Order/fChef.Designer.cs
@@ -0,0 +1,240 @@
+﻿namespace KTPOS_Order
+{
+    partial class fChef
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            Guna.UI2.WinForms.Guna2Button btnClose;
+            this.btnMaxSize = new Guna.UI2.WinForms.Guna2Button();
+            this.btnMinSize = new Guna.UI2.WinForms.Guna2Button();
+            this.btnMaterial = new Guna.UI2.WinForms.Guna2Button();
+            this.btnChat = new Guna.UI2.WinForms.Guna2Button();
+            this.btnProccessing = new Guna.UI2.WinForms.Guna2Button();
+            this.btnOrderList = new Guna.UI2.WinForms.Guna2Button();
+            btnClose = new Guna.UI2.WinForms.Guna2Button();
+            this.SuspendLayout();
+            // 
+            // btnClose
+            // 
+            btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            btnClose.Animated = true;
+            btnClose.AutoRoundedCorners = true;
+            btnClose.BackColor = System.Drawing.Color.Transparent;
+            btnClose.BackgroundImage = global::KTPOS_Order.Properties.Resources.xmark_solid;
+            btnClose.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            btnClose.BorderRadius = 11;
+            btnClose.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.ToogleButton;
+            btnClose.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            btnClose.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            btnClose.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            btnClose.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            btnClose.FillColor = System.Drawing.Color.Transparent;
+            btnClose.Font = new System.Drawing.Font("Segoe UI", 9F);
+            btnClose.ForeColor = System.Drawing.Color.Transparent;
+            btnClose.Location = new System.Drawing.Point(1132, 39);
+            btnClose.Name = "btnClose";
+            btnClose.Size = new System.Drawing.Size(25, 25);
+            btnClose.TabIndex = 24;
+            btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // btnMaxSize
+            // 
+            this.btnMaxSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMaxSize.BackColor = System.Drawing.Color.Transparent;
+            this.btnMaxSize.BackgroundImage = global::KTPOS_Order.Properties.Resources.window_maximize_solid;
+            this.btnMaxSize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMaxSize.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMaxSize.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnMaxSize.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMaxSize.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnMaxSize.FillColor = System.Drawing.Color.Transparent;
+            this.btnMaxSize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnMaxSize.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMaxSize.Location = new System.Drawing.Point(1090, 39);
+            this.btnMaxSize.Name = "btnMaxSize";
+            this.btnMaxSize.Size = new System.Drawing.Size(25, 25);
+            this.btnMaxSize.TabIndex = 26;
+            this.btnMaxSize.Click += new System.EventHandler(this.btnMaxSize_Click);
+            // 
+            // btnMinSize
+            // 
+            this.btnMinSize.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnMinSize.BackColor = System.Drawing.Color.Transparent;
+            this.btnMinSize.BackgroundImage = global::KTPOS_Order.Properties.Resources.window_restore_solid__1_;
+            this.btnMinSize.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnMinSize.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMinSize.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnMinSize.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMinSize.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnMinSize.FillColor = System.Drawing.Color.Transparent;
+            this.btnMinSize.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.btnMinSize.ForeColor = System.Drawing.Color.Transparent;
+            this.btnMinSize.Location = new System.Drawing.Point(1090, 39);
+            this.btnMinSize.Name = "btnMinSize";
+            this.btnMinSize.Size = new System.Drawing.Size(25, 25);
+            this.btnMinSize.TabIndex = 25;
+            this.btnMinSize.Click += new System.EventHandler(this.btnMinSize_Click);
+            // 
+            // btnMaterial
+            // 
+            this.btnMaterial.Animated = true;
+            this.btnMaterial.AutoRoundedCorners = true;
+            this.btnMaterial.BackColor = System.Drawing.Color.Transparent;
+            this.btnMaterial.BorderRadius = 25;
+            this.btnMaterial.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.btnMaterial.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnMaterial.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnMaterial.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnMaterial.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnMaterial.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnMaterial.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnMaterial.FillColor = System.Drawing.Color.Transparent;
+            this.btnMaterial.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnMaterial.ForeColor = System.Drawing.Color.White;
+            this.btnMaterial.HoverState.BorderColor = System.Drawing.Color.White;
+            this.btnMaterial.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnMaterial.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnMaterial.HoverState.ForeColor = System.Drawing.Color.Maroon;
+            this.btnMaterial.Location = new System.Drawing.Point(314, 12);
+            this.btnMaterial.Name = "btnMaterial";
+            this.btnMaterial.Size = new System.Drawing.Size(216, 52);
+            this.btnMaterial.TabIndex = 35;
+            this.btnMaterial.Text = "Material Management";
+            this.btnMaterial.UseTransparentBackground = true;
+            // 
+            // btnChat
+            // 
+            this.btnChat.Animated = true;
+            this.btnChat.AutoRoundedCorners = true;
+            this.btnChat.BackColor = System.Drawing.Color.Transparent;
+            this.btnChat.BorderRadius = 25;
+            this.btnChat.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.btnChat.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnChat.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnChat.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnChat.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnChat.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnChat.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnChat.FillColor = System.Drawing.Color.Transparent;
+            this.btnChat.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnChat.ForeColor = System.Drawing.Color.White;
+            this.btnChat.HoverState.BorderColor = System.Drawing.Color.White;
+            this.btnChat.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnChat.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnChat.HoverState.ForeColor = System.Drawing.Color.Maroon;
+            this.btnChat.Location = new System.Drawing.Point(564, 12);
+            this.btnChat.Name = "btnChat";
+            this.btnChat.Size = new System.Drawing.Size(102, 52);
+            this.btnChat.TabIndex = 34;
+            this.btnChat.Text = "Chat";
+            this.btnChat.UseTransparentBackground = true;
+            // 
+            // btnProccessing
+            // 
+            this.btnProccessing.Animated = true;
+            this.btnProccessing.AutoRoundedCorners = true;
+            this.btnProccessing.BackColor = System.Drawing.Color.Transparent;
+            this.btnProccessing.BorderRadius = 25;
+            this.btnProccessing.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.btnProccessing.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnProccessing.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnProccessing.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnProccessing.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnProccessing.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnProccessing.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnProccessing.FillColor = System.Drawing.Color.Transparent;
+            this.btnProccessing.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnProccessing.ForeColor = System.Drawing.Color.White;
+            this.btnProccessing.HoverState.BorderColor = System.Drawing.Color.White;
+            this.btnProccessing.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnProccessing.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnProccessing.HoverState.ForeColor = System.Drawing.Color.Maroon;
+            this.btnProccessing.Location = new System.Drawing.Point(151, 12);
+            this.btnProccessing.Name = "btnProccessing";
+            this.btnProccessing.Size = new System.Drawing.Size(130, 52);
+            this.btnProccessing.TabIndex = 33;
+            this.btnProccessing.Text = "Proccessing";
+            this.btnProccessing.Click += new System.EventHandler(this.btnProccessing_Click);
+            // 
+            // btnOrderList
+            // 
+            this.btnOrderList.Animated = true;
+            this.btnOrderList.AutoRoundedCorners = true;
+            this.btnOrderList.BackColor = System.Drawing.Color.Transparent;
+            this.btnOrderList.BorderRadius = 25;
+            this.btnOrderList.BorderStyle = System.Drawing.Drawing2D.DashStyle.Dash;
+            this.btnOrderList.ButtonMode = Guna.UI2.WinForms.Enums.ButtonMode.RadioButton;
+            this.btnOrderList.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnOrderList.DisabledState.BorderColor = System.Drawing.Color.DarkGray;
+            this.btnOrderList.DisabledState.CustomBorderColor = System.Drawing.Color.DarkGray;
+            this.btnOrderList.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(169)))), ((int)(((byte)(169)))), ((int)(((byte)(169)))));
+            this.btnOrderList.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(141)))), ((int)(((byte)(141)))), ((int)(((byte)(141)))));
+            this.btnOrderList.FillColor = System.Drawing.Color.Transparent;
+            this.btnOrderList.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold);
+            this.btnOrderList.ForeColor = System.Drawing.Color.White;
+            this.btnOrderList.HoverState.BorderColor = System.Drawing.Color.White;
+            this.btnOrderList.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.btnOrderList.HoverState.FillColor = System.Drawing.Color.White;
+            this.btnOrderList.HoverState.ForeColor = System.Drawing.Color.Maroon;
+            this.btnOrderList.Location = new System.Drawing.Point(12, 12);
+            this.btnOrderList.Name = "btnOrderList";
+            this.btnOrderList.Size = new System.Drawing.Size(117, 52);
+            this.btnOrderList.TabIndex = 32;
+            this.btnOrderList.Text = "Order List";
+            this.btnOrderList.Click += new System.EventHandler(this.btnOrderList_Click);
+            // 
+            // fChef
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.BackgroundImage = global::KTPOS_Order.Properties.Resources.bground;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1169, 740);
+            this.Controls.Add(this.btnMaterial);
+            this.Controls.Add(this.btnChat);
+            this.Controls.Add(this.btnProccessing);
+            this.Controls.Add(this.btnOrderList);
+            this.Controls.Add(this.btnMaxSize);
+            this.Controls.Add(this.btnMinSize);
+            this.Controls.Add(btnClose);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Name = "fChef";
+            this.Text = "fChef";
+            this.Load += new System.EventHandler(this.fChef_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2Button btnMaxSize;
+        private Guna.UI2.WinForms.Guna2Button btnMinSize;
+        private Guna.UI2.WinForms.Guna2Button btnMaterial;
+        private Guna.UI2.WinForms.Guna2Button btnChat;
+        private Guna.UI2.WinForms.Guna2Button btnProccessing;
+        private Guna.UI2.WinForms.Guna2Button btnOrderList;
+    }
+}
